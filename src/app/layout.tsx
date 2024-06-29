c84bb673ab7169814afd82b7285e37b5eac141c5
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-06-29 22:27:45 +0530

    Add signin button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       )}>
         <Providers>
           <Navbar />
-          <div className="min-h-[calc(100vh-64px)]">{children}</div>zz
+          <div className="min-h-[calc(100vh-64px)] justify-center items-center flex">{children}</div>
         </Providers>
       </body>
     </html>
